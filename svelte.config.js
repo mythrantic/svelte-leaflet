ecf9cdb82737f2441088c10a083eb758faf6e7d2
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-09-18 15:16:57 +0200

    ready for gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,14 @@
 import adapterAuto from '@sveltejs/adapter-auto';
 import adapterNode from '@sveltejs/adapter-node';
 import adapterStatic from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import dotenv from 'dotenv'
 dotenv.config()
 
-let {DEPLOY_TARGET} = process.env
-const deployTarget = DEPLOY_TARGET || 'auto'; // Default to 'auto'
+let {PUBLIC_DEPLOY_TARGET} = process.env
+const deployTarget = PUBLIC_DEPLOY_TARGET || 'auto'; // Default to 'auto'
+const dev = process.argv.includes('dev');
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,11 +17,18 @@ const config = {
 	preprocess: [vitePreprocess({})],
 
 	kit: {
+		appDir: 'app', // Required as the default is _app
+
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: deployTarget === 'auto' ? adapterAuto() : (deployTarget === 'static' ? adapterStatic() : adapterNode())
+		adapter: deployTarget === 'auto' ? adapterAuto() : deployTarget === 'static' ? adapterStatic() : adapterNode(),
+		paths: {
+            base: dev ? '' : process.env.BASE_PATH,
+        },
+
 	}
 };
 
 export default config;
+
